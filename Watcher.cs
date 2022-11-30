3293a0d6885c55a452cfb12cddaa3fc2d4272dda
Martin <schmart@gmx.de>
2022-11-30 20:32:21 +0100

    asp.net core server

--- Watcher.cs
+++ Watcher.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Routing.Constraints;
+using System.Web;
 
 namespace berrywatch
 {
@@ -14,29 +16,29 @@ namespace berrywatch
     public class Watcher
     {
         private FileSystemWatcher watcher;
+        private HttpClient wc;
+
+        [Option('u', "url", Required = false, HelpText = "Host local server on this url", Default=@"http://192.168.178.101:5001")]
+        public string ServerUrl { get; set; }
+
 
         [Option('d', "device", Required = true, HelpText = "IP/Adress of tasmota device")]
         public string DeviceAddress { get; set; }
 
-        [Option('f', "folder", Required = true, HelpText = "Folder to watch", Default = ".")]
+        [Option('f', "folder", Required = true, HelpText = "Folder to watch", Default = @"C:\temp\berry\")]
         public string Folder { get; set; }
 
         [Option("filter", Required = false, HelpText = "Filter for files to watch", Default = "*.be")]
         public string Filter { get; set; }
+
         public int Run()
         {
-            return Task.Run(async () => {
-                return await this.RunAsync();
-            }).Result;
-        }
-
-        private async Task<int> RunAsync()
-        {
             this.watch();
-            while (true)
-            {
-                await Task.Delay(100);
-            }
+            this.wc = new HttpClient();
+            this.wc.BaseAddress = new Uri($"http://{this.DeviceAddress}");
+            var srv = new Server();
+            srv.Run(this.ServerUrl, this.Folder);
+            return 0;
         }
 
         private void watch()
@@ -51,16 +53,36 @@ namespace berrywatch
             watcher.Created += OnCreated;
             watcher.EnableRaisingEvents = true;
         }
+        private async void uploadFile(string path)
+        {
+            Task.Run(async () => {
+                await this.uploadFileAsync(path); 
+            }).Wait(3000);
+        }
 
+        private async Task uploadFileAsync(string path)
+        {
+            Console.WriteLine($"Upload file {path}");
+
+            await this.RunTasmotaCommand($"UrlFetch {ServerUrl}/prj/{path}");
+        }
+
+        private async Task RunTasmotaCommand(string cmd)
+        {
+            var encoded = HttpUtility.UrlEncode(cmd);
+            var request = $"/cm?cmnd={encoded}";
+            Console.Write(request);
+            var result = await this.wc.GetAsync(request);
+            Console.WriteLine(" " +result.StatusCode);
+        }
         private void OnCreated(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine($"File {e.Name} created - {e.ChangeType}");
+            this.uploadFile(e.Name);
         }
 
         private void OnChanged(object source, FileSystemEventArgs e)
         {
-            Console.WriteLine($"File {e.Name} modified - {e.ChangeType}");
-            //Copies file to another directory.
+            this.uploadFile(e.Name);
         }
 
         public void Dispose()
