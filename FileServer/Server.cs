d212852e42a69dbe209a5ac7b9ad6af497da2ddd
Martin <schmart@gmx.de>
2022-11-30 20:40:39 +0100

    add server

---
+++ FileServer/Server.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+
+public class Server
+{
+    public void Run(string url, string path)
+    {
+        var args = new List<string>() {
+            @"--urls",url
+        };
+        var builder = WebApplication.CreateBuilder(args.ToArray());
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDirectoryBrowser();
+
+        var app = builder.Build();
+       
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+        var fileProvider = new PhysicalFileProvider(path, ExclusionFilters.None);
+        var requestPath = "/prj";
+
+        var provider = new FileExtensionContentTypeProvider();
+        // Add new mappings
+        provider.Mappings[".be"] = "text/javascript";
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            OnPrepareResponse = (ctx) =>
+            {
+                Console.WriteLine(ctx.File.Name);
+            },
+            ContentTypeProvider = provider,
+            FileProvider = fileProvider,
+            RequestPath = requestPath
+        });
+
+        app.UseDirectoryBrowser(new DirectoryBrowserOptions
+        {
+            FileProvider = fileProvider,
+            RequestPath = requestPath
+        });
+
+        app.UseAuthorization();
+
+        app.MapDefaultControllerRoute();
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
